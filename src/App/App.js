9d93a2120f4003623d7f0cbb2496b13e7878821a
Victória Freitas <victoriafreitas@cvccorp.com.br>
2023-10-20 17:51:27 -0300

    feat(): added react webcam implementation

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
 import WebcamComponent from "../components/WebcamComponent/WebcamCompoent";
+import { TextareaAutosize } from "@mui/material";
+
 
 const App = () => {
   return (
-    <div className="App">
-      <WebcamComponent />
-    </div>
+    <>
+      <div className="App">
+        <WebcamComponent />
+      </div>
+      <TextareaAutosize className="TextArea" minRows={20}/>
+    </>
   );
 }
 
